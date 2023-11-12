8dbb4ad720f2a18943843f4edcb458e0d3c71b1d
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-11-12 17:55:13 +0900

    에디터 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,13 +9,13 @@ export function App() {
 					CarbonCoder
 				</Toolbar>
 			</AppBar> */}
-      <Box style={{ display: 'flex', justifyContent: 'center', margin: 16}}>
-        <Typography variant="h1">
+      <Box style={{ display: 'flex', justifyContent: 'center', margin: 16 }}>
+        <Typography variant='h1'>
           CarbonCoder
         </Typography>
       </Box>
       <Box>
-        <Card style={{ flex: 1, margin: 16 }}>
+        <Card style={{ margin: 16, flexDirection: 'column' }}>
           <Tabs value={0}>
             <Tab label='코드 분석' value={0} />
             <Tab label='그린 패턴 더보기' value={1} />
--- front/src/analyser/Analyser.tsx
+++ front/src/analyser/Analyser.tsx
@@ -1,10 +1,36 @@
-import { Grid } from "@mui/material";
-import React from 'react';
+import { Box, Button, Card, Divider, Stack } from "@mui/material";
+import { useState } from "react";
+import Editor from '@monaco-editor/react'
+
+const defaultVal = `class Main {
+    public static void main(String[] args) {
+        System.out.println("코드를 입력하세요!");
+    }
+}`;
 
 export function Analyser() {
+  const [code, setCode] = useState(defaultVal);
+
+  const sendCode = () => {
+    console.log(code);
+  }
+
 	return (
-		<Grid>
-			
-		</Grid>
+		<Stack spacing={2} style={{ margin: 16 }}>
+      <Stack spacing={2} direction='row'>
+        <Editor height='500px' defaultLanguage='java' onChange={(v) => setCode(v ?? '')} defaultValue={defaultVal} />;
+        <Card style={{ padding: 8 }}>
+          1234
+        </Card>
+      </Stack>
+      <Divider/>
+      <Stack spacing={2} direction='row'>
+        <Button onClick={sendCode} variant='contained'>STDIN 추가하기</Button>
+        <Button onClick={sendCode} variant='contained'>분석하기</Button>
+      </Stack>
+      <Box style={{ flex: 1 }}>
+
+      </Box>
+    </Stack>
 	);
 }
