4a09107c5968e651c2c7bec87fb84ba13af09102
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-11-28 02:57:55 +0900

    정보 표시카드 추가

--- front/src/analyser/Analyser.tsx
+++ front/src/analyser/Analyser.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Divider, Stack } from "@mui/material";
+import { Box, Button, Card, Divider, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import Editor from '@monaco-editor/react'
 
@@ -15,15 +15,53 @@ export function Analyser() {
     console.log(code);
   }
 
-	return (
-		<Stack spacing={2} style={{ margin: 16 }}>
+  return (
+    <Stack spacing={2} style={{ margin: 16 }}>
       <Stack spacing={2} direction='row'>
-        <Editor height='500px' defaultLanguage='java' onChange={(v) => setCode(v ?? '')} defaultValue={defaultVal} />;
-        <Card style={{ padding: 8 }}>
-          1234
+        <Box style={{flex: 1}}>
+          <Editor height='500px' defaultLanguage='java' onChange={(v) => setCode(v ?? '')} defaultValue={defaultVal} theme="vs-dark"/>
+        </Box>
+        <Card style={{ padding: 16 }}>
+          <Stack spacing={2}>
+            <Typography variant='h6'>코드 실행 환경 하드웨어</Typography>
+            <TextField
+              label="코어 모델"
+              value="AMD EPYC 7702P 64-Core Processor"
+              InputProps={{
+                readOnly: true,
+              }}
+              variant="standard"
+            />
+            <Stack spacing={2} direction='row'>
+              <TextField
+                label="코어 개수"
+                value="4"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+              <TextField
+                label="코어 타입"
+                value="???????"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+            </Stack>
+            <TextField
+              label="RAM"
+              value="8 GB"
+              InputProps={{
+                readOnly: true,
+              }}
+              variant="standard"
+            />
+          </Stack>
         </Card>
       </Stack>
-      <Divider/>
+      <Divider />
       <Stack spacing={2} direction='row'>
         <Button onClick={sendCode} variant='contained'>STDIN 추가하기</Button>
         <Button onClick={sendCode} variant='contained'>분석하기</Button>
@@ -32,5 +70,5 @@ export function Analyser() {
 
       </Box>
     </Stack>
-	);
+  );
 }
