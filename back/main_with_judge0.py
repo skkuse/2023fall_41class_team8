2218e57955b137797e2f0eec4234b86a7030a027
root <root@DESKTOP-C9VJPDJ>
2023-11-30 20:20:48 +0900

    feat: 실행 횟수 조정 및 계산 함수 추가 실행 횟수 조정: "number_of_runs": 1 -> 10 계산 함수 추가: calculation_energy_and_carbon()

--- back/main_with_judge0.py
+++ back/main_with_judge0.py
@@ -16,6 +16,7 @@ def request_to_judge0(code : str):
     request_json = { "source_code": code,
     "language_id": 62,
     "stdin": "Judge0",
+    "number_of_runs" : 10,  # defalut : 1
     # "cpu_time_limit": null,
     # "cpu_extra_time": null,
     # "wall_time_limit": null,
@@ -36,7 +37,23 @@ def delete_submission(token : str):
     result = requests.delete(judge0_url+"submissions/"+token)
     print(result.text)
     return result
-    
+
+def calculate_energy_and_carbon(cpu_time: float, memory: int):
+    n_core = 1  # number of processor
+    tdp = 12.5  # TDP(W): Core type: AMD EPYC 7702P 64-core processor, 200W for 64 cores. Our server has 4 cores.
+    u_core = 1  # Usage of core(%): when cannot identify use 1 for value, [0,1] : 1 = 100%
+    p_memory = memory * 0.0000003752  #  power consume per memory usage(W): 0.3725W/1GB memory usage
+    pue = 1  # Power Usage Effectiveness(%), when cannot identify use 1 for value
+    carbon_intensity = 0.41130 # CI(gCO2e/kWh): Use 2023 stat of S.Korea
+
+    energy = time * 3600 * (n_core * tdp * u_core + p_memory) * pue * 0.001  # (kWh), Energy consumption when used for 1 hour 
+    carbon_footprint = energy * carbon_intensity  # (gCO2e), CO2e Emission on 1h with energy source of S.Korea
+
+    return {
+        "energy": energy,
+        "carbon_footprint": carbon_footprint
+    }
+
 @app.route("/runjava", methods=["POST"])
 def runCode():
     code = request.get_data(as_text=True)
@@ -44,7 +61,14 @@ def runCode():
     while True:
         time.sleep(0.2)
         result = check_submission(token)
-        if result.json()["status"]["id"]!=2:
+        if result.json()["status"]["id"]!=2:  # if Processing, continue
+            if result.json()["status"]["id"]==3:  # Accepted
+                cpu_time = result.json()["cpu_time"]
+                memory = result.json()["memory"]
+                calculation_result = calculate_energy_and_carbon(cpu_time, memory)
+
+                result['energy'] = calculation_result['energy']
+                result['carbon_footprint'] = calculation_result['carbon_footprint']
             break
     delete_submission(token)
     return result.text
