c8d489f3e749bd6001477694c6c5e54bf47efb9a
soomin9106 <soomin9106@naver.com>
2023-11-30 20:16:58 +0900

    tab 변경

--- front/src/green/Green.tsx
+++ front/src/green/Green.tsx
@@ -1,6 +1,28 @@
+import { List, Paper, Stack } from "@mui/material"
+import mockData from "./mock"
+
 const Green = () => {
+    console.log(mockData);
+    
     return (
-        <></>
+        <Stack style={{ paddingTop: "16px", paddingLeft: "16px" }} direction="row" spacing={2}>
+            <Paper style={{ flex: 1, borderRadius: '12px', height: "100vh" }}>
+                <List
+                >
+                    <>
+                        {mockData.map((item) => {
+                            <div onClick={() => console.log(item)
+                            }>
+                                <span style={{ color: "black" }}>{item.name}</span>
+                            </div>
+
+                        })}
+                    </>
+                    
+                </List>
+            </Paper>
+            <Paper style={{ flex: 9, borderRadius: '12px', height: "100vh" }}>상세 정보</Paper>
+        </Stack>
     )
 }
 
